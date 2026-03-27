a15b7c427a94ac57a54afbaf17535534105f3078
vincentbenzo <vincentbenzoni@gmail.com>
2026-03-27 00:55:04 -0700

    fix cpu and ram percentage

--- sketchybar/items/ram.sh
+++ sketchybar/items/ram.sh
@@ -1,17 +1,15 @@
-sketchybar --add item        ram.percent right              \
-           --set ram.percent icon.drawing=off                \
-                             label.font="$NERD_FONT:Mono:12.0" \
-                             label="RAM"                     \
-                             y_offset=-4                     \
-                             width=40                        \
-                             update_freq=2                   \
-                             script="$PLUGIN_DIR/ram.sh"     \
-                                                             \
-           --add item        ram.label right                 \
+sketchybar --add item        ram.label right                 \
            --set ram.label   label="RAM"                     \
                              label.font="$NERD_FONT:Mono:8.0" \
                              label.color=$GREY               \
                              icon.drawing=off                \
                              width=0                         \
                              y_offset=6                      \
-                             padding_left=-40
+                                                             \
+           --add item        ram.percent right               \
+           --set ram.percent icon.drawing=off                \
+                             label.font="$NERD_FONT:Mono:12.0" \
+                             y_offset=-4                     \
+                             width=40                        \
+                             update_freq=2                   \
+                             script="$PLUGIN_DIR/ram.sh"
